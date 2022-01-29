4c62bf5878d7dd4349934a9089fd04a05f1e59d0
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2022-01-30 00:27:41 +0300

    Displaying a simple object  with JSX in one <p>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import "./index.css";
+
+const myDetails = {
+  firstName: 'Adolphe',
+  lastName: 'Tresor',
+  age: 25,
+};
 
 function getTitle(title){
   return title;
@@ -6,11 +13,14 @@ function getTitle(title){
 
 function App() {
   return(
-    <div>
-      <h1>Hello {getTitle('React')}</h1>
+    // <div>
+    //   <h1>Hello {getTitle('React')}</h1>
 
-      <label htmlFor='search'>Search: </label>
-      <input id="search" type="text" />
+    //   <label htmlFor='search'>Search: </label>
+    //   <input id="search" type="text" />
+    // </div>
+    <div className='myDetails'>
+      <p>Hello, <br /> My name is {myDetails.firstName} {myDetails.lastName} and I am {myDetails.age} years old. </p>
     </div>
   );
 }
